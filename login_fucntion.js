016485e7c45f5fa01dd33149c5a87e7174ac9750
Earth2023 <131428329+Earth2023@users.noreply.github.com>
2023-04-28 16:51:32 +0800

    Add files via upload

--- login_fucntion.js
+++ login_fucntion.js
@@ -1,10 +1,10 @@
-function dl() {
+﻿function dl() {
         const a = document.getElementById("username").value;
         const b = document.getElementById("password").value;
         if (a == "administrator" && b == "administrator") {
             alert("你好，管理员！")
         }
-        else if (a == "Earth2023" && b == "Earth2023") {
+        else if (a == "2ca688d20df46f23b682215bd2ccd7e59227e7594d6596c91ca835c095287fa6" && b == "2ca688d20df46f23b682215bd2ccd7e59227e7594d6596c91ca835c095287fa6") {
             alert("你好，Earth2023！")
         }
         else {

03ca4e99d50402b87e1fcc797c5caa0979c6c1c6
Earth2023 <131428329+Earth2023@users.noreply.github.com>
2023-06-15 21:31:43 +0800

    Add files via upload

--- login_fucntion.js
+++ login_fucntion.js
@@ -1,4 +1,39 @@
-﻿function dl() {
+﻿var callback = function (args) {
+    console.log(args)
+    console.log('验证成功');
+};
+var expiredCallback = function (args) {
+    console.log(args)
+    console.log('验证过期');
+};
+var errorCallback = function (args) {
+    console.log(args)
+    console.log('验证失败');
+};
+
+var widgetId;
+var onloadCallback = function () {
+    widgetId = grecaptcha.render('robot', {
+        'sitekey': '6Lcf0Z0mAAAAAEygikGp6EX1JRgynQZFYymwGHZP',
+        'theme': 'light',
+        'size': 'compact',
+        'callback': callback,
+        'expired-callback': expiredCallback,
+        'error-callback': errorCallback
+    });
+};
+
+function getResponseFromRecaptcha() {
+    var responseToken = grecaptcha.getResponse(widgetId);
+    if (responseToken.length == 0) {
+        alert("人机验证失败。")
+        return 0;
+    } else {
+        dl();
+
+    }
+};
+function dl() {
         const a = sha512(document.getElementById("username").value);
         const b = sha512(document.getElementById("password").value);
         if (a == "df09aec85d056853f2d9da9c8627db3507f39820594efe303980ac45339f80e2e1430f0f7e639635e7f6b12d185367a3938eaa7b0f2f84cbd857a7375617affc" && b == "df09aec85d056853f2d9da9c8627db3507f39820594efe303980ac45339f80e2e1430f0f7e639635e7f6b12d185367a3938eaa7b0f2f84cbd857a7375617affc") {
---
+++ title.js
@@ -0,0 +1,16 @@
+﻿const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+    const repeatedGreetings = async () => {
+      await sleep(300)
+      document.title = "Earth"
+      await sleep(300)
+      document.title = "Earth2023"
+      await sleep(300)
+      document.title = "Earth2023个"
+      await sleep(300)
+      document.title = "Earth2023个人"
+      await sleep(300)
+      document.title = "Earth2023个人网"
+      await sleep(300)
+      document.title = "Earth2023个人网站"
+    }
+    repeatedGreetings()

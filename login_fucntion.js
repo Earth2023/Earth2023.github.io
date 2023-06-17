4ea4019e05dbd29bf17ab699159799a7c50612d4
Earth2023 <131428329+Earth2023@users.noreply.github.com>
2023-06-17 15:15:39 +0800

    Improvements to address Google reCAPTCHA
    Update login_ Function.js ---- Improvements to address Google reCAPTCHA

--- login_fucntion.js
+++ login_fucntion.js
@@ -26,7 +26,7 @@ var onloadCallback = function () {
 function getResponseFromRecaptcha() {
     var responseToken = grecaptcha.getResponse(widgetId);
     if (responseToken.length == 0) {
-        alert("人机验证失败。")
+        document.write("<h2>人机验证失败。</h2>")
         return 0;
     } else {
         dl();
@@ -37,10 +37,12 @@ function dl() {
         const a = sha512(document.getElementById("username").value);
         const b = sha512(document.getElementById("password").value);
         if (a == "df09aec85d056853f2d9da9c8627db3507f39820594efe303980ac45339f80e2e1430f0f7e639635e7f6b12d185367a3938eaa7b0f2f84cbd857a7375617affc" && b == "df09aec85d056853f2d9da9c8627db3507f39820594efe303980ac45339f80e2e1430f0f7e639635e7f6b12d185367a3938eaa7b0f2f84cbd857a7375617affc") {
-  	    window.open("https://www.earth2023.top/", "_self")
+            document.write("<h2>你好，Administrator！</h2>")
+  	        window.open("https://www.earth2023.top/", "_self")
         }
         else if (a == "57f8810c2880a7ff0977cf37e8b664abe4ef36f523cf959f612e3356e794135902238865daee0a0140e845aae2ded173d51048798f91779a9613ffded26743fd" && b == "57f8810c2880a7ff0977cf37e8b664abe4ef36f523cf959f612e3356e794135902238865daee0a0140e845aae2ded173d51048798f91779a9613ffded26743fd") {
-            window.open("https://www.earth2023.top/", "_self")
+            document.write("<h2>你好，Earth2023！</h2>")
+	        window.open("https://www.earth2023.top/", "_self")
         }
         else {
             alert("您的密码或账号有误，请重新输入。")
